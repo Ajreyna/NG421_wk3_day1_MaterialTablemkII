4b7c571898a07c0863155f5d3376e1ee2d13c598
ajreyna <ajreyna88@gmail.com>
2020-04-20 09:55:10 -0500

    refactored weird problems

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { IAmazonProducts } from './interfaces/iamazon-products';
+import {DATA} from './amazon-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AmazonClone';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'price', 'inStock'];
+  dataSource: MatTableDataSource<IAmazonProducts>;
+
+  ngOnInit(): void{
+    this.dataSource = new MatTableDataSource(DATA);
+
+  }
 }
